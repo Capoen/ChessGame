8dd9117027d75b3a8882a4a8f7da8eab1331ff97
Arthur Capoen <capoen97@gmail.com>
2020-09-23 18:18:50 -0300

    First Class, Position

---
+++ Chess-Game/src/boardgame/Position.java
@@ -0,0 +1,41 @@
+package boardgame;
+
+public class Position {
+
+	private Integer row;
+	private Integer column;
+
+	// Standard Constructor
+	public Position() {
+
+	}
+
+	// Constructor Using Fields
+	public Position(Integer row, Integer column) {
+		this.row = row;
+		this.column = column;
+	}
+
+	// Getters and Setters
+	public Integer getRow() {
+		return row;
+	}
+
+	public void setRow(Integer row) {
+		this.row = row;
+	}
+
+	public Integer getColumn() {
+		return column;
+	}
+
+	public void setColumn(Integer column) {
+		this.column = column;
+	}
+	
+	@Override
+	public String toString() {
+		return this.row + ", " + this.column;
+	}
+
+}
