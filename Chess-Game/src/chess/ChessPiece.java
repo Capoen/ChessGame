cb1d94bd559a2e90ff0263a1ab0fe465ab596982
Arthur Capoen <capoen97@gmail.com>
2020-09-25 12:15:50 -0300

    Chess Game

--- Chess-Game/src/chess/ChessPiece.java
+++ Chess-Game/src/chess/ChessPiece.java
@@ -2,26 +2,40 @@ package chess;
 
 import boardgame.Board;
 import boardgame.Piece;
+import boardgame.Position;
 
-public class ChessPiece extends Piece {
+public abstract class ChessPiece extends Piece {
 
-	// Atributes
 	private Color color;
+	private int moveCount;
 
-	// Standard Constructor
-	public ChessPiece() {
-
-	}
-
-	// Constructor using fields
 	public ChessPiece(Board board, Color color) {
 		super(board);
 		this.color = color;
 	}
 
-	// Getters and Setters
 	public Color getColor() {
 		return color;
 	}
+	
+	public int getMoveCount() {
+		return moveCount;
+	}
+	
+	public void increaseMoveCount() {
+		moveCount++;
+	}
 
+	public void decreaseMoveCount() {
+		moveCount--;
+	}
+
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position);
+	}
+	
+	protected boolean isThereOpponentPiece(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p != null && p.getColor() != color;
+	}
 }
